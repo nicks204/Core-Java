d3a66148db2bd1072164a8e4f59d049784aad300
Your Name <you@example.com>
2023-11-04 21:32:09 +0530

    first commit

---
+++ src/collections/MapExample.java
@@ -0,0 +1,52 @@
+package collections;
+
+import java.util.HashMap;
+
+public class MapExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		HashMap<String,Integer> map1 = new HashMap<String,Integer>();
+		
+		map1.put("Item1", 100);
+		map1.put("Item2", 200);
+		map1.put("Item3", 100);
+		map1.put("Item4", 50);
+		
+		System.out.println(map1);
+		
+		// update the value of already present item
+		map1.put("Item3", 300);
+		System.out.println(map1);
+		
+		// find the price of item4 from map
+		System.out.println("price of item4 is " + map1.get("Item4"));
+		
+		// remove item2 from the map
+		map1.remove("Item2");
+		System.out.println(map1);
+		
+		//iterate through map
+		for(int value:map1.values()) {
+			
+			System.out.println("value is " + value);
+		}
+		
+		for(String key:map1.keySet()) {
+			
+			System.out.println("The key is " + key);
+			System.out.println("The value is " + map1.get(key));
+		}
+		
+		// find out occurrence of each word in the string using hashmap
+		
+		String str1 = "clean world green world happy world";
+		
+		// clean 1
+		// green 1
+		// happy 1
+		// world 3
+	}
+
+}
---
+++ src/oops/HDFCBank.java
@@ -0,0 +1,7 @@
+package oops;
+
+public class HDFCBank extends Bank {
+	
+	int Acccount;
+
+}
